48096dc2ab3974027fc3a4d19438c1851617ac5c
Shaun <34657716+ST12345678910@users.noreply.github.com>
2022-02-25 13:26:13 -0800

    added notes to all docs;
    -removed some unnecesary components as well

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+//uses the class of the divs to save the information that the user fills in, and the id of the div as a key
+// when checking the local storage, the time blocks will appear in the key section as ex. "hour9"
+
 $(document).ready(function () {
   $('.saveInfo').on('click', function () {
     let event = $(this).siblings('.eventDesc').val();
@@ -5,6 +8,8 @@ $(document).ready(function () {
     localStorage.setItem(time, event);
     $.notify("Saved", "success");
   });
+
+// the current time is specified to an hour in a 24 hour clock
 
   function timeRefresh() {
     let timePresent = moment().hours();
@@ -14,6 +19,7 @@ $(document).ready(function () {
     $('.timeAt').each(function () {
       let divHour = parseInt($(this).attr('id').match(/hour(\d+)/)[1]);
 
+      // switch satement that compares the current time to the time section, past will turn grey, present red, and future green
       
       switch(timePresent != true) {
         case (divHour < timePresent):
@@ -31,6 +37,8 @@ $(document).ready(function () {
     });
   }
 
+//refresh all of the values in the rows every 5000 milliseconds, this can be increased as it is not neccesary to refresh so often, may use additional memory
+
   timeRefresh();
 
   let refreshInterval = setInterval(timeRefresh, 5000);
@@ -43,6 +51,8 @@ $(document).ready(function () {
   $('#hour15 .eventDesc').val(localStorage.getItem('hour15'));
   $('#hour16 .eventDesc').val(localStorage.getItem('hour16'));
   $('#hour17 .eventDesc').val(localStorage.getItem('hour17'));
+
+// using moment.js the date is displayed as Day, Month XXth
 
   $('#timeNow').text(moment().format('dddd, MMMM Do'));
 });
